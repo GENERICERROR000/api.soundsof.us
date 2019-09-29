4494d47f96a6e82ba12ddb8ea12f690160630b0b
Noah Kernis <nkernis@gmail.com>
2019-09-28 22:23:29 -0400

    can save audio to s3

--- routes/post.js
+++ routes/post.js
@@ -7,21 +7,23 @@ AWS.config.update(config.aws)
 const soundFolder = '../sounds/'
 
 exports.saveSound = (req, res) => {
-	// let id = saveToS3(sound)
+	let id = saveToS3(req.file.buffer)
 	// saveToDB(id)
-	let uploadLocation = __dirname + '/../sounds/' + `${new Date().getTime()}` + ".wav"
+	// let uploadLocation = __dirname + '/../sounds/' + `${new Date().getTime()}` + ".wav"
 
-	fs.writeFileSync(uploadLocation, Buffer.from(new Uint8Array(req.file.buffer))); // write the blob to the server as a file
-	res.sendStatus(200); //send back that everything went ok
+	// fs.writeFileSync(uploadLocation, Buffer.from(new Uint8Array(req.file.buffer))); // write the blob to the server as a file
+	// res.sendStatus(200); //send back that everything went ok
 }
 
-const saveToS3 = (sound) => {
+const saveToS3 = (buffer) => {
+	console.log("hit")
 	let s3bucket = new AWS.S3()
 
 	let params = {
-		Bucket: "soundsof.us",
-		Key: sound.name,
-		Body: sound.data
+		Bucket: "media.soundsof.us",
+		Key: "audio/" + `${new Date().getTime()}` + ".wav",
+		ACL: "public-read",
+		Body: Buffer.from(new Uint8Array(buffer))
 	}
 	
 	s3bucket.upload(params, function (err, data) {
