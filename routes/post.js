0ae64fef6fd6eee2b842bc4c5175e4fc66e91cff
Noah Kernis <nkernis@gmail.com>
2019-09-29 22:25:26 -0400

    removed db call

--- routes/post.js
+++ routes/post.js
@@ -7,46 +7,29 @@ AWS.config.update(config.aws)
 const soundFolder = '../sounds/'
 
 exports.saveSound = (req, res) => {
-	let id = saveToS3(req.file.buffer)
-	// saveToDB(id)
-	// let uploadLocation = __dirname + '/../sounds/' + `${new Date().getTime()}` + ".wav"
-
-	// fs.writeFileSync(uploadLocation, Buffer.from(new Uint8Array(req.file.buffer))); // write the blob to the server as a file
-	// res.sendStatus(200); //send back that everything went ok
+	let name = req.file.originalname + `+--+${new Date().getTime()}` + ".wav"
+	saveToS3(req.file.buffer, name, res)
+	
+	// let uploadLocation = __dirname + '/../sounds/' + req.file.originalname + `+--+${new Date().getTime()}` + ".wav"
+	// fs.writeFileSync(uploadLocation, Buffer.from(new Uint8Array(req.file.buffer)));
 }
 
-const saveToS3 = (buffer) => {
-	console.log("hit")
+const saveToS3 = (buffer, name, res) => {
 	let s3bucket = new AWS.S3()
 
 	let params = {
 		Bucket: "media.soundsof.us",
-		Key: "audio/" + `${new Date().getTime()}` + ".wav",
+		Key: "audio/" + name,
 		ACL: "public-read",
 		Body: Buffer.from(new Uint8Array(buffer))
 	}
 	
-	s3bucket.upload(params, function (err, data) {
-		if (err) console.log(err)
-		else console.log(data)
-	})
-
-
-	// return "id"
-}
-
-const saveToDB = (id) => {
-	let docClient = new AWS.DynamoDB.DocumentClient()
-
-	let params = {
-		TableName: "soundsofus",
-		Item: {
-			"id": id
+	s3bucket.upload(params, function (err) {
+		if (err) {
+			console.error("UNABLE TO ADD SOUND:", err)
+		} else {
+			console.log("SOUND ADDED TO DB AND S3")
+			res.sendStatus(201)
 		}
-	}
-
-	docClient.put(params, (err, data) => {
-		if (err) console.error("UNABLE TO ADD SOUND:", err)
-		else console.log("SOUND ADDED:", data)
 	})
 }
